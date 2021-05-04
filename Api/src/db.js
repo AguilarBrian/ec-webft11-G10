1cf97dd89e7af7cdace3e30d85752e8d41c0c917
zaratush <martinc.1@hotmail.com>
2021-05-04 13:28:56 -0300

    boilerplate

--- Api/src/db.js
+++ Api/src/db.js
@@ -7,11 +7,10 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/ecommerce11`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    operatorsAliases: false,
   }
 );
 const basename = path.basename(__filename);
