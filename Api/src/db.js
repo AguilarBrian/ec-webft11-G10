b4f42f7fc0384569804761174023979c332f912e
AguilarBrian <brianisaiasaguilar@gmail.com>
2021-05-05 12:32:19 -0300

    fixed backend routes

--- Api/src/db.js
+++ Api/src/db.js
@@ -1,8 +1,7 @@
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const fs = require("fs");
-const path = require("path");
-
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
 
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
@@ -18,20 +17,20 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, "/models"))
+fs.readdirSync(path.join(__dirname, '/models'))
   .filter(
-    (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    file =>
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
   )
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  .forEach(file => {
+    modelDefiners.push(require(path.join(__dirname, '/models', file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach((model) => model(sequelize));
+modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [
+let capsEntries = entries.map(entry => [
   entry[0][0].toUpperCase() + entry[0].slice(1),
   entry[1],
 ]);
@@ -41,16 +40,15 @@ sequelize.models = Object.fromEntries(capsEntries);
 // Para relacionarlos hacemos un destructuring
 const {
   Product,
-  // Categories,
+  Category,
   // User,
   // Order,
-  // Review,  
+  // Review,
 } = sequelize.models;
 
-
-// // PRODUCTS CATEGORIES
-// Product.belongsToMany(Categories, { through: 'Products_Categories', as: 'categories' });
-// Categories.belongsToMany(Product, { through: 'Products_Categories', as: 'products' });
+// PRODUCTS CATEGORIES
+Product.belongsToMany(Category, { through: 'Products_Categories' });
+Category.belongsToMany(Product, { through: 'Products_Categories' });
 
 // Product.belongsToMany(Order, { through: 'Products_Order'  });
 // Order.belongsToMany(Product, { through: 'Products_Order' });
@@ -62,13 +60,10 @@ const {
 // User.hasMany(Review, { foreignKey: "userId" });
 // Review.belongsTo(User);
 
-
 // User.hasMany(Order, { foreignKey: "userId" });
 // Order.belongsTo(User);
-
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
-
