29dc10db12c9dbb8eb561acf148ca6ef630b67e6
NuLuciano <69189864+NuLuciano@users.noreply.github.com>
2021-05-05 18:46:46 -0300

    Catalog component

--- Client/src/pages/Home.js
+++ Client/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import AppBar from '../components/AppBar/AppBar'
+import Catalog from '../components/Catalog/Catalog'
 export const Home = () => {
     return (
         <div>
             <AppBar/>
+            <Catalog/>
         </div>
     )
 }
