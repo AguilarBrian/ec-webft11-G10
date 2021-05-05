b159e84223ba387780f624608924a84e72094998
zaratush <martinc.1@hotmail.com>
2021-05-04 23:04:26 -0300

    firebase file upload

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,12 +2,14 @@ import React, {useState} from 'react';
 import { Route } from "react-router-dom";
 //import { userContext } from './components/userContext';
 import { Home } from './pages/Home';
+import  UploadImage  from './components/UploadImage';
 
 function App() {
   return (
     <React.Fragment>
       {/* <userContext.Provider value={{}}> */}
         <Route exact path='/' component={Home} />
+        <Route exact path='/upload' component={UploadImage} />
       {/* </userContext.Provider> */}
     </React.Fragment>
   );
