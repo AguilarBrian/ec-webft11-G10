27bfad812e76168cfe2828169d5db9946bf57e0e
NuLuciano <69189864+NuLuciano@users.noreply.github.com>
2021-05-05 16:42:39 -0300

    Componente de product card

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import { userContext } from './components/userContext';
 import { Home } from './pages/Home';
 import PageAddProduct from './pages/AddProduct/PageAddProduct'
+import ProductCard from './components/ProductCard/ProductCard'
 //import  UploadImage  from './components/UploadImage';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <userContext.Provider value={{}}>
         <Route exact path='/' component={Home} />
         <Route path='/createProduct' component={PageAddProduct} />
+        <Route path='/productcard' component={ProductCard} />
       </userContext.Provider>
     </React.Fragment>
   );
