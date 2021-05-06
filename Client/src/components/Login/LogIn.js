f00ed6f7587b59ce86edb495ec82068c558b6651
Esteban Cespedes <ces.esteban@gmail.com>
2021-05-05 23:36:15 -0300

    primerIntentodeLandPage

--- Client/src/components/Login/LogIn.js
+++ Client/src/components/Login/LogIn.js
@@ -1,13 +1,93 @@
 import React from 'react';
-import {makeStyles} from '@material-ui/core/styles'
-import { useStyles } from './styleLogIn';
+import { Form, Field } from 'react-final-form';
+import { TextField } from 'final-form-material-ui';
+import UploadImage from '../UploadImage';
+import { Paper, Grid, Button, CssBaseline, MenuItem } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
+const onSubmit = async values => {
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+    await sleep(300);
+    window.alert(JSON.stringify(values, 0, 2));
+};
 
-function LogIn(props) {
-    const classes = useStyles()
+const validate = values => {
+    const errors = {};
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    }
+    if (!values.lastName) {
+        errors.lastName = 'Required';
+    }
+    if (!values.email) {
+        errors.email = 'Required';
+    }
+    return errors;
+};
+
+function LogIn() {
+
     return (
-        <div className={classes.root}>
-            Landing Page
+        <div style={{ padding: 16, margin: 'auto', maxWidth: 600 }}>
+            <CssBaseline />
+            <Form
+                onSubmit={onSubmit}
+                initialValues={{}}
+                validate={validate}
+                render={({ handleSubmit, submitting, pristine, values }) => (
+                    <form onSubmit={handleSubmit} noValidate>
+                        <Paper style={{ padding: 16 }}>
+                            <Grid container alignItems="flex-start" spacing={6}>
+                                <Grid item xs={6}>
+                                    <Field
+                                        fullWidth
+                                        required
+                                        name="firstName"
+                                        component={TextField}
+                                        type="text"
+                                        label="Nombre"
+                                    />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Field
+                                        fullWidth
+                                        required
+                                        name="lastName"
+                                        component={TextField}
+                                        type="text"
+                                        label="Apellido"
+                                    />
+                                </Grid>
+                                <Grid item xs={6}>
+                                    <Field
+                                        name="email"
+                                        fullWidth
+                                        required
+                                        component={TextField}
+                                        type="email"
+                                        label="Email"
+                                    />
+                                </Grid>
+                                <Grid item
+                                    xs={12}
+                                    style={{ marginTop: 16 }}>
+                                    <Button
+                                        to="/Home"
+                                        component={Link}
+                                        variant="contained"
+                                        color="primary"
+                                        type="submit"
+                                        disabled={submitting}
+                                    >
+                                        LogIn
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                        <pre>{JSON.stringify(values, 0, 2)}</pre>
+                    </form>
+                )}
+            />
         </div>
     );
 }
--- Client/src/pages/landingPage/LandingPage.js
+++ Client/src/pages/landingPage/LandingPage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useStyles } from '../../pages/landingPage/styleLanding';
+import { CssBaseline } from '@material-ui/core';
 import LogIn from '../../components/login/LogIn'
 
 function PageLogIn(props) {
+    const classes = useStyles()
     return (
-        <div>
+        <div className={classes.root}>
+            <CssBaseline/>
             <LogIn/>
         </div>
     );
