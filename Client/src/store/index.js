7436f84092d713e552b9137a32b23be7d214fb2f
Esteban Cespedes <ces.esteban@gmail.com>
2021-05-05 22:28:40 -0300

    indexRedux

--- Client/src/store/index.js
+++ Client/src/store/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import productsReducers from "./product/reducer";
+import categoryReducers from "./category/category.reducer"
 export default combineReducers({
   productsReducers,
+  categoryReducers,
   
 });
