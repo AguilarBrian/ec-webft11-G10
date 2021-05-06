a33ea89b2fa08c5d28df7d7e459cda685475f04b
Esteban Cespedes <ces.esteban@gmail.com>
2021-05-05 21:29:49 -0300

    LoginLangdinPre

--- Client/src/App.js
+++ Client/src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { Route } from "react-router-dom";
 import { userContext } from './components/userContext';
 import { Home } from './pages/Home';
-import PageAddProduct from './pages/AddProduct/PageAddProduct'
-import ProductCard from './components/ProductCard/ProductCard'
-import PageAddCategory from './pages/AddCategory/PageAddCategory'
+import PageAddProduct from './pages/addProduct/PageAddProduct'
+import ProductCard from './components/productCard/ProductCard'
+import PageAddCategory from './pages/addCategory/PageAddCategory'
+import LandingPage from './pages/landingPage/LandingPage'
 //import  UploadImage  from './components/UploadImage';
 
 function App() {
   return (
     <React.Fragment>
       <userContext.Provider value={{}}>
-        <Route exact path='/' component={Home} />
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/home' component={Home} />
         <Route path='/createProduct' component={PageAddProduct} />
         <Route path='/productcard' component={ProductCard} />
         <Route path='/createCategory' component={PageAddCategory} />
---
+++ Client/src/pages/landingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogIn from '../../components/login/LogIn'
+
+function PageLogIn(props) {
+    return (
+        <div>
+            <LogIn/>
+        </div>
+    );
+}
+
+export default PageLogIn;
