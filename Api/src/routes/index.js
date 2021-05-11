61e828ef0d4b6d7d0f903a034e812414daf631d4
Esteban Cespedes <ces.esteban@gmail.com>
2021-05-11 00:58:29 -0300

    relacion carrito orden

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -2,18 +2,19 @@ const { Router } = require('express');
 const router = Router();
 
 
-const getProductsRouter = require("./products");
-const categoryRouter = require("./category");
+//const getProductsRouter = require("./products");
+//const categoryRouter = require("./category");
 // const userRouter= require("./Product/userRouter");
 // const ordersRouter= require("./Product/ordersRouter");
 
 
 
-router.use("/products", getProductsRouter);
-router.use("/category", categoryRouter);
+router.use("/products", require("./products"));
+router.use("/category", require("./category"));
 // router.use("/users", userRouter);
 // // router.use("/orders", ordersRouter);
 
 
+router.use('/cart', require('./cart.js'));
 
 module.exports = router;
