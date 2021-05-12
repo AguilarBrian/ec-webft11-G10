6bc710e180199dd5238b6fd457339aa04f67e12f
Esteban Cespedes <ces.esteban@gmail.com>
2021-05-12 13:25:58 -0300

    ruta devuelve producto segun id

--- Api/src/routes/cart.js
+++ Api/src/routes/cart.js
@@ -1,10 +1,18 @@
 const server = require('express').Router();
-const cart = require('../controllers/cart');
+const { Order, Product, User } = require("../db");
 
-server.get('/',(req,res,next)=>{
+//Devuelve productos segun userId------
+server.get('/:id',(req,res,next)=>{
+    const {id} = req.params
+    if(!id) return res.error()
     order.findAll({
         include:{
             model:product
+        },
+        where: {
+            userId:parseInt(id)
         }
     })
+    .then(res=>res.send(res))
+    .catch(error=>res.send(error))
 })
