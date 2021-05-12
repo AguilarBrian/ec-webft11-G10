c4e47710eea038a50b4fe417aabd1bc4f39b5202
Esteban Cespedes <ces.esteban@gmail.com>
2021-05-11 23:00:09 -0300

    Fix

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,40 +5,30 @@ import { Home } from './pages/Home';
 import PageAddProduct from './pages/adminProduct/PageAddProduct'
 import PageAdminCategory from './pages/adminCategory/PageAdminCategory'
 import LandingPage from './pages/landingPage/LandingPage'
-<<<<<<< HEAD
-// import PageCheckoutOrders from './pages/ordersCheck/PageCheckoutOrders'
-import Product from './components/Product/Product'
-=======
-import PageCheckoutOrders from './pages/ordersCheck/PageCheckoutOrders'
-import Product from './pages/ProductDetail'
->>>>>>> a830f56f64a6eddb56ca8ba1ca9cf5c59df88150
+import Product from './components/product/Product'
 import { AdminProduct } from './components/admin/AdminProduct';
 import EditProduct from './pages/adminProduct/EditProduct';
-import {ThemeProvider} from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/core/styles'
 import theme from './theme'
-<<<<<<< HEAD
 import PageCheckoutOrders from './pages/ordersCheck/PageCheckoutOrders'
 import ViewOrder from './components/admin/ViewOrder'
+import CartProducts from './pages/CartProducts'
 
-=======
-import CartProducts  from './pages/CartProducts';
->>>>>>> a830f56f64a6eddb56ca8ba1ca9cf5c59df88150
 function App() {
   return (
     <React.Fragment>
       <userContext.Provider value={{}}>
-      <ThemeProvider theme={theme}>
-      <Route exact path='/cart' component={CartProducts} />
-
-        <Route exact path='/' component={LandingPage} />
-        <Route exact path='/home' component={Home} />
-        <Route  path='/editProduct/:id' component={EditProduct} />
-        <Route  path='/adminProduct' component={AdminProduct} />
-        <Route path='/createProduct' component={PageAddProduct} />
-        <Route path='/adminCategories' component={PageAdminCategory} />
-        <Route path='/PageCheckoutOrders' component={PageCheckoutOrders} />
-        <Route path='/ViewOrder' component={ViewOrder} />
-        <Route path='/product/:id' component={Product} />
+        <ThemeProvider theme={theme}>
+          <Route exact path='/cart' component={CartProducts} />
+          <Route exact path='/' component={LandingPage} />
+          <Route exact path='/home' component={Home} />
+          <Route path='/editProduct/:id' component={EditProduct} />
+          <Route path='/adminProduct' component={AdminProduct} />
+          <Route path='/createProduct' component={PageAddProduct} />
+          <Route path='/adminCategories' component={PageAdminCategory} />
+          <Route path='/PageCheckoutOrders' component={PageCheckoutOrders} />
+          <Route path='/ViewOrder' component={ViewOrder} />
+          <Route path='/product/:id' component={Product} />
         </ThemeProvider>
       </userContext.Provider>
     </React.Fragment>
--- Client/src/store/user/user.action.js
+++ Client/src/store/user/user.action.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 const serverUrl = "http://localhost:3001/";
 
-
+export const GET_CART = "GET_CART";
+export const ADD_TO_CART = "ADD_TO_CART";
 export const GET_USER_BYID = "GET_USER_BYID"
 export const GET_USERS = "GET_USERS"
 export const POST_USER = "POST_USER";
@@ -9,15 +10,24 @@ export const PUT_USER = "PUT_USER";
 export const DELETE_USER = "DELETE_USER";
 export const SELECT_USER = "SELECT_USER";
 
-
-
-
-
-
 export const getUsersById = (id) => {
-  return function (dispatch) {
-    axios.get(`${serverUrl}users/${id}`).then((payload) => {console.log("esto es el user de id",payload)
-      dispatch({ type:GET_USER_BYID , payload: payload.data });
-    });
-  };
+    return function (dispatch) {
+        axios.get(`${serverUrl}users/${id}`).then((payload) => {
+            console.log("esto es el user de id", payload)
+            dispatch({ type: GET_USER_BYID, payload: payload.data });
+        });
+    };
 };
+export const getCart = (cart) => {
+    return {
+        type: GET_CART,
+        payload: cart
+    }
+}
+export const addToCart = (product) => {
+    localStorage.setItem('cart', JSON.stringify(product))
+    return {
+        type: ADD_TO_CART,
+        payload: product
+    }
+}
